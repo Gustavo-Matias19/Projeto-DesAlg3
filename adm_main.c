62958d80363cec883e9b74008429d7433f4da9e7
Gustavo-Matias19 <gustavomatias248@gmail.com>
2024-11-19 22:50:34 -0300

    adicionei a funçao extrato na adm

--- adm_main.c
+++ adm_main.c
@@ -5,19 +5,21 @@
 
 #include "cadastro.h"
 #include "funcoes.h"
-//#include "inv_trade.h"
+// #include "inv_trade.h"
 #include "adm_trade.h"
-
-int main(){
+void consultarExtratoADM(struct Investidores *investidores, int qtd_investidores);
+int main()
+{
     // defs para adms
     struct Adms adm;
     int logado = 0;
     cadastrar_adm(&adm);
 
-    while (!logado) {
+    while (!logado)
+    {
         logado = login(&adm);
     }
-    
+
     // Def para as criptos
     int qtd_moedas = 0;
     struct Criptomoeda criptos[MAX_CRIPTOS];
@@ -29,7 +31,8 @@ int main(){
     ler_invest_txt(investidor, &qtd_investidor);
 
     int opcao, continuar = 1;
-    while (continuar) {
+    while (continuar)
+    {
         printf("\n=-=-= Menu de Administrador =-=-=\n");
         printf("1. Cadastrar novo investidor\n");
         printf("2. Excluir investidor\n");
@@ -40,42 +43,43 @@ int main(){
         printf("7. Atualizar cotacao de criptomoedas\n");
         printf("8. Sair\n");
         printf("Digite sua opcao: ");
-        if(scanf("%d", &opcao) != 1){
+        if (scanf("%d", &opcao) != 1)
+        {
             fprintf(stderr, "Erro ao ler valor, tente novamente\n");
             clearBuffer();
             continue;
         };
         clearBuffer();
 
-        switch (opcao) {
-            case 1:
-                cadastrar_investidor(investidor, &qtd_investidor);
-                break;
-            case 2:
-                remover_investidor(investidor, &qtd_investidor);
-                break;
-            case 3:
-                adicionar_criptomoeda(criptos, &qtd_moedas);
-                break;
-            case 4:
-                remover_criptomoeda(criptos,&qtd_moedas);
-                break;
-            case 5:
-                consultar_saldo_invest(investidor, &qtd_investidor);
-                break;
-            case 6:
-                printf("Consultar extrato de investidor\n");
-                break;
-            case 7:
-                atualizar_cotacao(criptos, &qtd_moedas);
-                break;
-            case 8:
-                printf("Saindo...\n");
-                continuar = 0;
-                break;
-            default:
-                printf("Opcao invalida.\n");
+        switch (opcao)
+        {
+        case 1:
+            cadastrar_investidor(investidor, &qtd_investidor);
+            break;
+        case 2:
+            remover_investidor(investidor, &qtd_investidor);
+            break;
+        case 3:
+            adicionar_criptomoeda(criptos, &qtd_moedas);
+            break;
+        case 4:
+            remover_criptomoeda(criptos, &qtd_moedas);
+            break;
+        case 5:
+            consultar_saldo_invest(investidor, &qtd_investidor);
+            break;
+        case 6:
+            consultarExtratoADM(investidor, qtd_investidor);
+            break;
+        case 7:
+            atualizar_cotacao(criptos, &qtd_moedas);
+            break;
+        case 8:
+            printf("Saindo...\n");
+            continuar = 0;
+            break;
+        default:
+            printf("Opcao invalida.\n");
         }
     }
-
 }
